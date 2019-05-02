1ff04fa714dc404233e0ff5ac2f54b97d605c4ff
RegisWeill <regis_weill@hotmail.com>
2019-05-03 01:25:05 +0200

    pages controller + home

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'about', to: 'pages#about', as: :about
+  get 'contact', to: 'pages#contact', as: :contact
+
+  root to: 'pages#home', as: :home
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
